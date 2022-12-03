d6de20d0735754d5552244503dbe624f6d333f21
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-04 00:30:38 +0500

    (fix): bug in redis pubSub retryStrategy

--- src/pubSub.ts
+++ src/pubSub.ts
@@ -9,9 +9,13 @@ export default function bootstrap(): PubSubEngine {
     const options: RedisOptions = {
         host: env.REDIS_HOST,
         port: env.REDIS_PORT,
-        retryStrategy: times => Math.max(times * env.REDIS_RETRY_LIMIT, env.REDIS_RETRY_TIMEOUT),
+        retryStrategy: times =>
+            times > env.REDIS_RETRY_LIMIT ?
+                null
+                : Math.max(times * env.REDIS_RETRY_TIMEOUT, env.REDIS_RETRY_TIMEOUT)
+        ,
     }
-    const errorHandler = (type: string, message: string) => logger.log('error',`redis: ${type} failed to connect, error: ${message}`)
+    const errorHandler = (type: string, message: string) => logger.log('error', `redis: ${type} failed to connect, error: ${message}`)
     const pubSub = new RedisPubSub({
         publisher: new Redis(options).on('error', (error: Error) => errorHandler('publisher', error.message)),
         subscriber: new Redis(options).on('error', (error: Error) => errorHandler('subscriber', error.message))
