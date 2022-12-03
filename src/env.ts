1a82aa84699309496aa72152d44dc82536cf5978
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-03 18:06:12 +0500

    add common redis and jwt envars

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,13 @@
-import { cleanEnv, num } from 'envalid'
-
-const env = cleanEnv(process.env,{
-    PORT: num({default: 4000})
+import { cleanEnv, num, str } from 'envalid'
+const isDevelopment = process.env.NODE_ENV === 'development'
+const env = cleanEnv(process.env, {
+    PORT: num({ default: 4000 }),
+    JWT_SECRET: str({ default: isDevelopment ? 'mysecret' : null }),
+    SALT_ROUNDS: num({ default: 10 }),
+    REDIS_HOST: str({ default: isDevelopment ? 'localhost' : null }),
+    REDIS_PORT: num({ default: 6379 }),
+    REDIS_RETRY_LIMIT: num({ default: isDevelopment ? 1 : 100 }),
+    REDIS_RETRY_TIMEOUT: num({ default: 3000 })
 })
 
 export default env;
